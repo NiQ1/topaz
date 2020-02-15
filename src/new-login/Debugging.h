6f229bc366a261013eac29d5778a0f0b446cfb03
Dan Haim <danhaim@danhaim.com>
2020-02-15 23:06:04 +0200

    indentation issue fixed

--- src/new-login/Debugging.h
+++ src/new-login/Debugging.h
@@ -16,50 +16,50 @@ extern "C"
 {
 #endif
 
-	/// Severity level of logs, controls how much debug info is printed
-	typedef enum _LOG_SEVERITY_LEVEL
-	{
-		LOG_SEVERITY_DEBUG0 = 0,
-		LOG_SEVERITY_DEBUG1 = 1,
-		LOG_SEVERITY_INFO = 2,
-		LOG_SEVERITY_WARNING = 3,
-		LOG_SEVERITY_ERROR = 4,
-		LOG_SEVERITY_CRITICAL = 5,
-		LOG_SEVERITY_LAST
-	} LOG_SEVERITY_LEVEL, *PLOG_SEVERITY_LEVEL;
+/// Severity level of logs, controls how much debug info is printed
+typedef enum _LOG_SEVERITY_LEVEL
+{
+	LOG_SEVERITY_DEBUG0 = 0,
+	LOG_SEVERITY_DEBUG1 = 1,
+	LOG_SEVERITY_INFO = 2,
+	LOG_SEVERITY_WARNING = 3,
+	LOG_SEVERITY_ERROR = 4,
+	LOG_SEVERITY_CRITICAL = 5,
+	LOG_SEVERITY_LAST
+} LOG_SEVERITY_LEVEL, *PLOG_SEVERITY_LEVEL;
 
-	/// Log level threshold, only prints this level or above will actually get logged
-	extern LOG_SEVERITY_LEVEL geLogLevel;
+/// Log level threshold, only prints this level or above will actually get logged
+extern LOG_SEVERITY_LEVEL geLogLevel;
 
-	/// File to log into, in addition to stderr
-	extern FILE* ghLogFile;
+/// File to log into, in addition to stderr
+extern FILE* ghLogFile;
 
-	/**
-	 *	Print a message to the log file specified with var_args
-	 *	@param szFileName File name of the source code calling file
-	 *	@param iLineNo Line number in the source code
-     *  @param szFuncName Calling function name
-	 *	@param eSeverity Severity level of the log message
-	 *	@param pszFormat printf compatible format string
-	 *	@param vaArgs Message arguments
-	 *	@return Number of bytes written to log
-	 */
-	int LogPrintV(const char* szFileName, int iLineNo, const char* szFunctionName, LOG_SEVERITY_LEVEL eSeverity, const char* pszFormat, va_list vaArgs);
+/**
+	*	Print a message to the log file specified with var_args
+	*	@param szFileName File name of the source code calling file
+	*	@param iLineNo Line number in the source code
+    *  @param szFuncName Calling function name
+	*	@param eSeverity Severity level of the log message
+	*	@param pszFormat printf compatible format string
+	*	@param vaArgs Message arguments
+	*	@return Number of bytes written to log
+	*/
+int LogPrintV(const char* szFileName, int iLineNo, const char* szFunctionName, LOG_SEVERITY_LEVEL eSeverity, const char* pszFormat, va_list vaArgs);
 
-	/**
-	 *	Print a message to the log file
-	 *	@param szFileName File name of the source code calling file
-	 *	@param iLineNo Line number in the source code
-     *  @param szFuncName Calling function name
-     *	@param eSeverity Severity level of the log message
-	 *	@param pszFormat printf compatible format string
-	 *	@param ... Message arguments
-	 *	@return Number of bytes written to log
-	 */
-	int LogPrint(const char* szFileName, int iLineNo, const char* szFunctionName, LOG_SEVERITY_LEVEL eSeverity, const char* pszFormat, ...);
+/**
+	*	Print a message to the log file
+	*	@param szFileName File name of the source code calling file
+	*	@param iLineNo Line number in the source code
+    *  @param szFuncName Calling function name
+    *	@param eSeverity Severity level of the log message
+	*	@param pszFormat printf compatible format string
+	*	@param ... Message arguments
+	*	@return Number of bytes written to log
+	*/
+int LogPrint(const char* szFileName, int iLineNo, const char* szFunctionName, LOG_SEVERITY_LEVEL eSeverity, const char* pszFormat, ...);
 
 
-	// Easy to use macro definitions
+// Easy to use macro definitions
 #if _DEBUG || DEBUG || ENABLE_DEBUG_LOGS
 #define LOG_MESSAGE(sev, msg, ...) LogPrint(__FILE__, __LINE__, __FUNCTION__, sev, msg, ##__VA_ARGS__)
 #else
